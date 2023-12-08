0ff3e5d83f23a859d04895d4725eeae130fa8125
SpecialOne969 <139588579+SpecialOne969@users.noreply.github.com>
2023-12-08 11:19:23 +0100

    Update custom.js

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -34,9 +34,9 @@
 	const popoverTriggerList = document.querySelectorAll(
 		'[data-bs-toggle="popover"]'
 	);
-	// const popoverList = [...popoverTriggerList].map(
-	// 	(popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-	// );
+	const popoverList = [...popoverTriggerList].map(
+		(popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+	);
 
 	//switcher color pickers
 	const pickrContainerPrimary = document.querySelector(
